b85fd2bcc652ab68ec4f0506ae2eebe9452c808c
Sebastian Herholz <sebastian.herholz@intel.com>
2021-04-14 18:00:20 +0200

    [cmake] extended cmake scripts to build a static C library

---
+++ cmake/openpgl_macros.cmake
@@ -0,0 +1,48 @@
+## Copyright 2021 Intel Corporation
+## SPDX-License-Identifier: Apache-2.0
+
+macro(openpgl_create_embree_target)
+  if (NOT TARGET embree)
+    add_library(embree INTERFACE) # NOTE(jda) - Cannot be IMPORTED due to CMake
+                                  #             issues found on Ubuntu.
+
+    target_include_directories(embree
+    INTERFACE
+      $<BUILD_INTERFACE:${EMBREE_INCLUDE_DIRS}>
+    )
+
+    target_link_libraries(embree
+    INTERFACE
+      $<BUILD_INTERFACE:${EMBREE_LIBRARY}>
+    )
+  endif()
+endmacro()
+
+
+macro(openpgl_install_library name)
+  set_target_properties(${name}
+    PROPERTIES VERSION ${PROJECT_VERSION} SOVERSION ${PROJECT_VERSION_MAJOR})
+  openpgl_install_target(${name})
+endmacro()
+
+macro(openpgl_install_target name)
+  install(TARGETS ${name}
+    EXPORT ${PROJECT_NAME}_Exports
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+      NAMELINK_SKIP
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  )
+
+  install(EXPORT ${PROJECT_NAME}_Exports
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}-${PROJECT_VERSION}
+    NAMESPACE openpgl::
+  )
+
+  install(TARGETS ${name}
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+      NAMELINK_ONLY
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  )
+endmacro()
