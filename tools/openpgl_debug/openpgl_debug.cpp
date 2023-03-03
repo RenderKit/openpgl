5e2b864f722f39898502f9f1fc2169279240a868
Sebastian Herholz <sebastian.herholz@intel.com>
2023-03-03 13:37:16 +0100

    [debug] Adding "exportSamplesToOBJ" functionality

--- tools/openpgl_debug/openpgl_debug.cpp
+++ tools/openpgl_debug/openpgl_debug.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <list>
 #include <fstream>
+#include <vector>
 #include <iostream>
 
 enum DebugType{
@@ -13,6 +14,7 @@ enum DebugType{
     UPDATE_FIELD,
     VALIDATE_FIELD,
     VALIDATE_SAMPLES,
+    EXPORT_SAMPLES,
     NONE
 };
 
@@ -26,6 +28,7 @@ struct DebugParams {
     DebugType type {NONE};
     std::string field_file_name {""};
     std::string samples_file_name {""};
+    std::string obj_out_file_name {""};
 
     bool validate() {
         bool valid = true;
@@ -61,7 +64,18 @@ struct DebugParams {
                     valid = false;
                 }
                 break;
-            
+            case EXPORT_SAMPLES:
+                if(samples_file_name == "" || 
+                    !file_exists(samples_file_name)) {
+                        std::cout << "ERROR: Samples file not set or does not exists: " << samples_file_name << std::endl;
+                    valid = false;
+                }
+                if(obj_out_file_name == "") {
+                        std::cout << "ERROR: OBJ output file not set: " << obj_out_file_name << std::endl;
+                    valid = false;
+                }
+                break;
+
             case NONE:
                 valid = false;
                 break;
@@ -93,11 +107,11 @@ bool parseCommandLine(std::list<std::string> &args,
                     debugParams.type = DebugType::VALIDATE_FIELD; 
                 } else if(str_type == "validateSamples") {
                     debugParams.type = DebugType::VALIDATE_SAMPLES; 
-                //} else if(str_type == "update") {
-                
+                } else if(str_type == "exportSamplesToOBJ") {
+                    debugParams.type = DebugType::EXPORT_SAMPLES; 
                 } else {
                     std::cout << "ERROR: Unknown type: " << str_type << std::endl;
-                    std::cout << "       Valid types are: [updateField validateField validateSamples] "<< std::endl;
+                    std::cout << "       Valid types are: [updateField validateField validateSamples exportSamplesToOBJ] "<< std::endl;
                     return false;
                 }
             } else {
@@ -121,6 +135,15 @@ bool parseCommandLine(std::list<std::string> &args,
             } else {
                 return false;
             }
+        } else if (arg == "-out") {
+            ++it;
+            if(it != args.end())
+            {
+                const std::string str_samples = *it;
+                debugParams.obj_out_file_name = str_samples; 
+            } else {
+                return false;
+            }
         }
         ++it;
     }
@@ -132,7 +155,7 @@ void print_help(){
 }
 
 void update_field(DebugParams &debugParams){
-    openpgl::cpp::Device device(PGL_DEVICE_TYPE_CPU_8);
+    openpgl::cpp::Device device(PGL_DEVICE_TYPE_CPU_4);
     openpgl::cpp::Field field(&device, debugParams.field_file_name);
     openpgl::cpp::SampleStorage sampleStorage(debugParams.samples_file_name);
 
@@ -150,6 +173,63 @@ void validate_field(DebugParams &debugParams){
 
 void validate_samples(DebugParams &debugParams){
 
+}
+
+
+void export_samples(DebugParams &debugParams){
+    std::ofstream objFile;
+    objFile.open(debugParams.obj_out_file_name.c_str());
+    
+    openpgl::cpp::SampleStorage sampleStorage(debugParams.samples_file_name);
+
+    bool pointsOnly = true;
+    std::vector<openpgl::cpp::SampleData> subSampledData;
+    subSampledData.reserve(sampleStorage.GetSizeSurface() + sampleStorage.GetSizeVolume());
+    for (size_t i =0; i < sampleStorage.GetSizeSurface(); i++)
+    {
+        subSampledData.push_back(sampleStorage.GetSampleSurface(i));
+    }
+    for (size_t i =0; i < sampleStorage.GetSizeVolume(); i++)
+    {
+        subSampledData.push_back(sampleStorage.GetSampleVolume(i));
+    }
+
+    for (auto& sample : subSampledData)
+    {
+        objFile << "v " << sample.position.x << "\t" << sample.position.y << "\t"<< sample.position.z << std::endl;
+        if (!pointsOnly)
+        {
+            pgl_vec3f dir = sample.direction;
+            pgl_vec3f samplePosition = sample.position;
+            pgl_vec3f pos2;
+            pos2.x = samplePosition.x + dir.x * sample.distance;
+            pos2.y = samplePosition.y + dir.y * sample.distance;
+            pos2.z = samplePosition.z + dir.z * sample.distance;
+            objFile << "v " << pos2.x << "\t" << pos2.y << "\t"<< pos2.z << std::endl;
+            objFile << "v " << sample.position.x << "\t" << sample.position.y << "\t"<< sample.position.z << std::endl;
+        }
+    }
+
+    for (auto& sample : subSampledData)
+    {
+        pgl_vec3f dir = sample.direction;
+        objFile << "vn " << dir.x << "\t" << dir.y << "\t"<< dir.z << std::endl;
+        if (!pointsOnly)
+        {
+            objFile << "vn " << dir.x << "\t" << dir.y << "\t"<< dir.z << std::endl;
+            objFile << "vn " << dir.x << "\t" << dir.y << "\t"<< dir.z << std::endl;
+        }
+    }
+
+    if (!pointsOnly)
+    {
+        for (int i = 0; i < subSampledData.size(); i++)
+        {
+            objFile << "f " << i*3+1 << "\t" << i*3+2 << "\t"<< i*3+3 << std::endl;
+        }
+    }
+
+    objFile.close();
 }
 
 
@@ -179,7 +259,9 @@ int main (int argc, char *argv[]) {
         case VALIDATE_SAMPLES:
             validate_samples(debugParams);
             break;
-                        
+         case EXPORT_SAMPLES:
+            export_samples(debugParams);
+            break;                       
         default:
             print_help();
             break;
