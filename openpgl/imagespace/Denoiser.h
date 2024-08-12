4c87dc3dc991e7eafe53f6e0798951382433f35b
Herholz, Sebastian <sebastian.herholz@intel.com>
2024-08-12 10:54:32 +0200

    [denoiser] Switching to in-place denoising and removing unnecessary OIDN buffers

--- openpgl/imagespace/Denoiser.h
+++ openpgl/imagespace/Denoiser.h
@@ -23,29 +23,23 @@ struct Denoiser{
         bufferScalar = oidnDevice.newBuffer(numPixels * 1 * sizeof(float));
         bufferAlbedo = oidnDevice.newBuffer(numPixels * 3 * sizeof(float));		
         bufferNormal = oidnDevice.newBuffer(numPixels * 3 * sizeof(float));
-
-        bufferColor3fOutput = oidnDevice.newBuffer(numPixels * 3 * sizeof(float));
-        bufferScalarOutput = oidnDevice.newBuffer(numPixels * 1 * sizeof(float));
         
         if (filterFeatures) {
-            bufferNormalOutput = oidnDevice.newBuffer(numPixels * 3 * sizeof(float));
-            bufferAlbedoOutput = oidnDevice.newBuffer(numPixels * 3 * sizeof(float));
-
             oidnAlbedoFilter = oidnDevice.newFilter("RT");
             oidnAlbedoFilter.setImage("albedo", bufferAlbedo, oidn::Format::Float3, m_resolution.x, m_resolution.y);
-            oidnAlbedoFilter.setImage("output", bufferAlbedoOutput, oidn::Format::Float3, m_resolution.x, m_resolution.y);
+            oidnAlbedoFilter.setImage("output", bufferAlbedo, oidn::Format::Float3, m_resolution.x, m_resolution.y);
             oidnAlbedoFilter.commit();
                 
             oidnNormalFilter = oidnDevice.newFilter("RT");
             oidnNormalFilter.setImage("normal", bufferNormal, oidn::Format::Float3, m_resolution.x, m_resolution.y);
-            oidnNormalFilter.setImage("output", bufferNormalOutput, oidn::Format::Float3, m_resolution.x, m_resolution.y);
+            oidnNormalFilter.setImage("output", bufferNormal, oidn::Format::Float3, m_resolution.x, m_resolution.y);
             oidnNormalFilter.commit();
 
             oidnColor3fFilter = oidnDevice.newFilter("RT");
             oidnColor3fFilter.setImage("color", bufferColor3f, oidn::Format::Float3, m_resolution.x, m_resolution.y);
-            oidnColor3fFilter.setImage("albedo", bufferAlbedoOutput, oidn::Format::Float3, m_resolution.x, m_resolution.y);
-            oidnColor3fFilter.setImage("normal", bufferNormalOutput, oidn::Format::Float3, m_resolution.x, m_resolution.y);
-            oidnColor3fFilter.setImage("output", bufferColor3fOutput, oidn::Format::Float3, m_resolution.x, m_resolution.y);
+            oidnColor3fFilter.setImage("albedo", bufferAlbedo, oidn::Format::Float3, m_resolution.x, m_resolution.y);
+            oidnColor3fFilter.setImage("normal", bufferNormal, oidn::Format::Float3, m_resolution.x, m_resolution.y);
+            oidnColor3fFilter.setImage("output", bufferColor3f, oidn::Format::Float3, m_resolution.x, m_resolution.y);
             oidnColor3fFilter.set("cleanAux", true); // auxiliary images will be prefiltered
             oidnColor3fFilter.set("hdr", true);
             oidnColor3fFilter.commit();
@@ -54,7 +48,7 @@ struct Denoiser{
             oidnColor3fFilter.setImage("color", bufferColor3f, oidn::Format::Float3, m_resolution.x, m_resolution.y);
             oidnColor3fFilter.setImage("albedo", bufferAlbedo, oidn::Format::Float3, m_resolution.x, m_resolution.y);
             oidnColor3fFilter.setImage("normal", bufferNormal, oidn::Format::Float3, m_resolution.x, m_resolution.y);
-            oidnColor3fFilter.setImage("output", bufferColor3fOutput, oidn::Format::Float3, m_resolution.x, m_resolution.y);
+            oidnColor3fFilter.setImage("output", bufferColor3f, oidn::Format::Float3, m_resolution.x, m_resolution.y);
 
             oidnColor3fFilter.set("hdr", true);
             oidnColor3fFilter.commit();
@@ -62,7 +56,7 @@ struct Denoiser{
 
         oidnScalarFilter = oidnDevice.newFilter("RT");
         oidnScalarFilter.setImage("color", bufferScalar, oidn::Format::Float, m_resolution.x, m_resolution.y);
-        oidnScalarFilter.setImage("output", bufferScalarOutput, oidn::Format::Float, m_resolution.x, m_resolution.y);
+        oidnScalarFilter.setImage("output", bufferScalar, oidn::Format::Float, m_resolution.x, m_resolution.y);
         oidnScalarFilter.set("hdr", true);
         oidnScalarFilter.commit();
     }
@@ -80,7 +74,7 @@ struct Denoiser{
             oidnNormalFilter.execute();
         }
         oidnColor3fFilter.execute();
-        bufferColor3fOutput.read(0, numPixels*3*sizeof(float), result);
+        bufferColor3f.read(0, numPixels*3*sizeof(float), result);
     }
 
     void denoise(pgl_vec3f *rgb, pgl_vec3f *rgb2nd, pgl_vec3f *n, pgl_vec3f *albedo, pgl_vec3f *result, pgl_vec3f *result2nd)
@@ -95,11 +89,11 @@ struct Denoiser{
             oidnNormalFilter.execute();
         }
         oidnColor3fFilter.execute();
-        bufferColor3fOutput.read(0, numPixels*3*sizeof(float), result);
+        bufferColor3f.read(0, numPixels*3*sizeof(float), result);
 
         bufferColor3f.write(0, numPixels*3*sizeof(float), rgb2nd);
         oidnColor3fFilter.execute();
-        bufferColor3fOutput.read(0, numPixels*3*sizeof(float), result2nd);
+        bufferColor3f.read(0, numPixels*3*sizeof(float), result2nd);
     }
 
     void denoise(float *l, float *result)
@@ -107,7 +101,7 @@ struct Denoiser{
         const std::size_t numPixels = m_resolution.x * m_resolution.y;
         bufferScalar.write(0, numPixels*1*sizeof(float), l);
 	    oidnScalarFilter.execute();
-	    bufferScalarOutput.read(0, numPixels*1*sizeof(float), result);
+	    bufferScalar.read(0, numPixels*1*sizeof(float), result);
     }
 
     private:
@@ -122,16 +116,9 @@ struct Denoiser{
     oidn::FilterRef oidnNormalFilter;
 
     oidn::BufferRef bufferColor3f;
-    oidn::BufferRef bufferColor3fOutput;
-
     oidn::BufferRef bufferScalar;
-    oidn::BufferRef bufferScalarOutput;
-
     oidn::BufferRef bufferAlbedo;
-    oidn::BufferRef bufferAlbedoOutput;
-
     oidn::BufferRef bufferNormal;
-    oidn::BufferRef bufferNormalOutput;
 };
 
 }
