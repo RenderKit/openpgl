6682fab75d61c2522c4723a8074c15cdf81506af
Herholz, Sebastian <sebastian.herholz@intel.com>
2024-09-18 18:21:03 +0200

    [clang-format] Adding ./tools/openpgl-viewer/third-party to the ignore list

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clang-format -style=file -i -fallback-style=none $(git ls-files -- *.cpp *.h ':!:./third-party')
+clang-format -style=file -i -fallback-style=none $(git ls-files -- *.cpp *.h ':!:./third-party' ':!:./tools/openpgl-viewer/third-party')
